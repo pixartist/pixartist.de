232e929c2115ccf5ccfc9be91265516f74f7535d
pixartist <pixartist.de@gmail.com>
2023-12-16 18:37:39 +0100

    added api token

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Bearer authentication middleware
+  app.use((req, res, next) => {
+    const token = process.env.BEARER_TOKEN;
+    const authHeader = req.headers.authorization;
+
+    if (authHeader && authHeader.startsWith('Bearer ') && authHeader.slice(7) === token) {
+      next();
+    } else {
+      res.status(401).send('Unauthorized').end();
+    }
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
