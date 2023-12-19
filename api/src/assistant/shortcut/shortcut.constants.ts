450512165e299dc18884b59dfaca8dc28ccee032
pixartist <pixartist.de@gmail.com>
2023-12-19 16:31:13 +0100

    promt changes

--- api/src/assistant/shortcut/shortcut.constants.ts
+++ api/src/assistant/shortcut/shortcut.constants.ts
@@ -3,12 +3,15 @@ export class ShortcutConstants {
 
   static readonly INSTRUCTIONS = `You can run some of my iOS shortcuts.
 Provided is a list with available shortcut names, brackets indicated possible named parameters.
- You can use the function "run_shortcut" to directly solve the task, or use it to run other shortcuts first if you are missing information.
+You can use the function "run_shortcut" to directly solve the task, or use it to run other shortcuts first if you are missing information.
+ALWAYS use the function "run_shortcut" to reply, unless the conversation is over.s
 If you run a shortcut, I will provide you with the ouput of the shortcut.
 Use the shortcut names exactly as provided including the parameter names.
 Don't remove parameters from shortcut names. 
 Slashes in parameter names denote possible values for that parameter.
-Put the named parameters and their values into the params object.`;
+Put the named parameters and their values into the params object.
+If you have to ask me any question or need additional information, use the prompt shortcut.
+I might ask in German but the shortcuts are all in english, translate if necessary.`;
   static readonly MODEL = 'gpt-4';
   static readonly NAME = 'Shortcut Assistant';
   static readonly FUNCTION = {
